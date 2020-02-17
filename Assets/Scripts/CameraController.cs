629478aa47ad4d254123be5f539444bb251a49ff
Paul Cupido <paul@touchfoundry.co.za>
2020-02-17 22:28:11 +0200

    Created a custom camera to track the player as they move

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null) {
+            target = PlayerController.instance.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y,transform.position.z);
+    }
+}
