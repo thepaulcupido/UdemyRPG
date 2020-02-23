e4e89b6943575d558c7ae95b35f85f3961ec4fd2
Paul Cupido <paul@touchfoundry.co.za>
2020-02-23 15:28:39 +0200

    Changed country side to Town and create new country side scene

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
 
+    // Public variables
+    public Tilemap map;
     public Transform target;
+
+    // Private variables
+    private Vector3 topRightLimit;
+    private Vector3 bottomLeftLimit;
+
+    private float halfWidth;
+    private float halfHeight;
 
     // Start is called before the first frame update
     void Start()
@@ -13,11 +23,24 @@ public class CameraController : MonoBehaviour
         if (target == null) {
             target = PlayerController.instance.transform;
         }
+        
+        this.halfHeight = Camera.main.orthographicSize;
+        this.halfWidth = halfHeight * Camera.main.aspect;
+
+        this.topRightLimit = map.localBounds.max + new Vector3(-halfWidth, -halfHeight, 0f);
+        this.bottomLeftLimit = map.localBounds.min + new Vector3(halfWidth, halfHeight, 0f);
+
+        PlayerController.instance.SetBounds(map.localBounds.min, map.localBounds.max);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x, target.position.y,transform.position.z);
+
+        // Keep the camera inside the bounds
+        var boundsX = Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x); 
+        var boundsY = Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y);
+        transform.position = new Vector3(boundsX, boundsY, transform.position.z);
     }
 }
