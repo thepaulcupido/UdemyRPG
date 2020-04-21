87dc3a0cc87d19d54fa09bc47cc401ac76517208
Paul Cupido <paul@touchfoundry.co.za>
2020-04-21 21:08:40 +0200

    Created DialogBox and script to allow for dialog interactions

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+
+    public Text dialogText;
+    public Text nameText;    
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    public string[] dialogLines;
+
+    public int currentLine;
+
+    void Start()
+    {
+        if (dialogLines.Length > 0) {
+            dialogText.text = dialogLines[0];
+        }
+
+    }
+
+    void NextLine() {
+        if ((currentLine + 1) < dialogLines.Length) {
+            currentLine++;
+            dialogText.text = dialogLines[currentLine];
+        } else {
+            dialogBox.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+
+        if (dialogBox.active) {
+            // That Fire1 should really be moved into a config file and replaced with a generic variable
+            if (Input.GetButtonUp("Fire1")) {
+                NextLine();
+            }
+        }
+
+    }
+}
