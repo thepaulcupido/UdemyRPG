e435912495c2b56eaad9d8320796bf61168a6aa3
Paul Cupido <paul@touchfoundry.co.za>
2020-05-21 21:54:15 +0200

    Added AudioManager script and Audio objects to scene

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource[] sfx;
+    public AudioSource[] bgMusic;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            PlaySFX(0);
+        }
+    }
+
+    public void PlaySFX(int sound)
+    {
+        if (sound < sfx.Length) {
+            sfx[sound].Play();
+        }
+    }
+
+    public void PlayBackgroundMusic(int music)
+    {
+        StopMusic();
+        if (music < bgMusic.Length) {
+            bgMusic[music].Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        for (int i = 0; i < bgMusic.Length; i ++) {
+            bgMusic[i].Stop();
+        }
+    }
+}
