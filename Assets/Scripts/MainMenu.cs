eb119170f82b1bece14a63160c4bb8402b912064
Paul Cupido <paul@touchfoundry.co.za>
2020-05-25 21:19:36 +0200

    Added functionality to 'new game' and 'exit game' buttons

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string newGameScene;
+    public GameObject continueButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool hasKey = PlayerPrefs.HasKey("X");
+        continueButton.SetActive(hasKey);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Continue()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void ExitGame()
+    {  
+        // Will only work on build
+        Application.Quit();
+    }
+}
