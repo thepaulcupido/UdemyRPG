9a54cb55f069f77e822019713e2031bc055fe745
Paul Cupido <paul@touchfoundry.co.za>
2020-05-31 17:44:42 +0200

    -Added a BattleManager script for battles

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public static BattleManager instance;
+    public GameObject battleScene;
+    public Transform[] playerPositions;
+    public Transform[] transformPositions;
+
+    public BattleCharacter[] playerPrefabs;
+    public BattleCharacter[] enemyPrefabs;
+
+    // private variables
+    private bool isBattleActive;
+
+    void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N)) {
+            string[] enemies = {"Skeleton", "Spider"};
+            BattleStart(enemies);
+        }
+    }
+
+    public void BattleStart(string[] enemiesToSpawn)
+    {
+        if (!isBattleActive) {
+            
+            isBattleActive = GameManager.instance.isBattleActive = true;
+            transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, transform.position.z);
+            battleScene.SetActive(true);
+            AudioManager.instance.PlayBackgroundMusic(0);
+        }
+    }
+}
